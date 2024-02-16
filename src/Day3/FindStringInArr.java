fa25eef32310195274e776a13e1707730fcdf0ce
utkarsh.pandit <utkarsh.pandit@TLG-4ZYHMT2.techlensglobal.lan>
2024-02-16 11:34:23 +0530

    Day3 of Strings

--- src/Day3/FindStringInArr.java
+++ src/Day3/FindStringInArr.java
@@ -1,7 +1,42 @@
 package Day3;
+import java.util.Scanner;
 
 public class FindStringInArr {
 
-	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String s[] = new String[5];
+		boolean res = false;
+		
+		System.out.println("Enter 5 String : ");
+		
+		for(int i=0;i<s.length;i++)
+		{
+			s[i] = sc.next();
+		}
+		
+		System.out.print("Enter The String U Want To Search : "); //ram
+		String search = sc.next();
+		
+		for(String find:s)
+		{
+			if(search.equalsIgnoreCase(find)) //equalsignorecase means even if we write string in caps it will going to compare it ignore case sensitive  
+			{
+				res = true;
+			}
+		}
+		
+		if(res)
+		{
+			System.out.println("Searched String is present in array!");
+		}
+		else {
+			System.out.println("Searched String is not present in array!");
+		}
+		
+		
+		sc.close();
+	}
 	
 }
