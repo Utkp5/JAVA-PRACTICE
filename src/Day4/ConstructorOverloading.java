998932451aecaf6292422ed99c584f37b214422b
utkarsh.pandit <utkarsh.pandit@TLG-4ZYHMT2.techlensglobal.lan>
2024-02-19 13:52:52 +0530

    Day4

--- src/Day4/ConstructorOverloading.java
+++ src/Day4/ConstructorOverloading.java
@@ -2,4 +2,40 @@ package Day4;
 
 public class ConstructorOverloading {
 
+	
+	int x,y;
+	double z;
+	
+	ConstructorOverloading()
+	{
+		x=10;y=20;
+	}
+	
+	ConstructorOverloading(int x,int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	
+	ConstructorOverloading(int x,double z)
+	{
+		this.x=x;
+		this.z=z;
+	}
+	
+	void display()
+	{
+		System.out.println(x + " " + y + " " + z);
+	}
+	
+	public static void main(String[] args) {
+		
+		ConstructorOverloading co = new ConstructorOverloading();
+		ConstructorOverloading co1 = new ConstructorOverloading(500,100);
+		co.display();
+		co1.display();
+		
+		
+	}
+	
 }
