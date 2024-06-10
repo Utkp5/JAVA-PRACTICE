8e4f8ec82372d00fa6b09b49c7b3caccfadd5b07
Utkp5 <utkp200@gmail.com>
2024-06-10 18:37:54 +0530

    Day1

---
+++ src/Day1/Average_Prime_no.java
@@ -0,0 +1,51 @@
+package Day1;
+import java.util.*;
+
+public class Average_Prime_no {
+
+	//Code to find the average of prime no from a given range
+	
+	public static void main(String a[]) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the Number till where u want Prime Numbers : ");
+		int input = sc.nextInt();		
+		
+		int count=0,countprimeno = 0;
+		float avg = 0, sum = 0;
+		
+		for(int i=2; i<=input; i++)
+		{
+			
+			for(int j=1; j<=input; j++)
+			{
+				
+				if(i%j==0)
+				{
+					count++;
+				}
+				
+			}
+			if(count<=2)
+			{
+				System.out.print(i+" ");
+				countprimeno++;
+			}
+			count=0;
+			sum = sum + i;
+		}
+		avg = sum/countprimeno;
+		System.out.println();
+		System.out.println("CountPrimeNo : "+countprimeno);
+		System.out.println("Sum of Prime No. is :" + sum);
+		System.out.print("Average of Prime No. is :"+avg);
+		
+		
+		
+		
+		
+	}
+	
+}
