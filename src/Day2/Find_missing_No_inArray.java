71e53d04e0366b90ac1d286909ed0df64f2d087f
Utkp5 <utkp200@gmail.com>
2024-06-15 21:44:36 +0530

    Day2

---
+++ src/Day2/Find_missing_No_inArray.java
@@ -0,0 +1,61 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class Find_missing_No_inArray {
+
+	//Find the missing no in Array like ex:-   ******pending*******
+	/*
+	   number should be in range  + should not have duplicate no.s 
+	   logic ex:- {1,3,4,5,6} ---in this 2 is missing 
+	   so to find that we have a logic 
+	   1+2+3+4+5 = 21
+	   1+3+4+5+6 = 19
+	   21-19 = 2 (here is your ans)
+	   
+	 */
+	
+	public static void name(String a[]) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Integer arr[] = new Integer[5];
+		System.out.println("Enter the elements in Array : ");
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=1; j>=i; j++)
+			{
+				if(arr[j]-arr[i]==1 || arr[j]-arr[i]==2)
+				{
+					System.out.println("entered element in array is correct");
+				}
+			}
+		}
+
+		
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
